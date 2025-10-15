6a88cceb535bf7af28aaacc1bc8d5f6ed41ff3f9
Sharan Gouda <Sharanpatil106@gmail.com>
2025-10-15 11:29:16 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+
+const basename = import.meta.env.VITE_BASENAME || "";
+
 createRoot(document.getElementById('root')!).render(
  
     <BrowserRouter basename={basename}>
