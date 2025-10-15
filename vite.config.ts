e0c681855f06fb1ea77012f373594eb416df3e9d
Sharan Gouda <Sharanpatil106@gmail.com>
2025-10-15 11:14:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Book-Management-System/', // Only needed for GitHub Pages
+  base: './',
   plugins: [react()],
-})
+});
+
